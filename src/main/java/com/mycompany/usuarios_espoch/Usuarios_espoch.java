5ff54a3734d1436741ccb5befbbcd581423e59eb
unknown <fernandaminga42@gmail.com>
2024-11-25 14:16:31 -0500

    Mi primer tarea

---
+++ src/main/java/com/mycompany/usuarios_espoch/Usuarios_espoch.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.usuarios_espoch;
+
+/**
+ *
+ * @author Lizeth
+ */
+public class Usuarios_espoch {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
